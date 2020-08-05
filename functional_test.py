47892ef09f1dec6fa747beae3061d6aa0dc4c221
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2020-08-05 11:49:07 -0400

    Second commit - built a unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_createAndRetrieveList(self):
+        #Check out the homepage
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title, 'Wrong title')
+        #browser = webdriver.Firefox()
+
+        #browser.get('http://localhost:8000')
+
+
+        #To do is in the title
+        #assert 'To-Do' in browser.title
+
+
+        #SHe can enter an item
+
+        #ENter Buy Peacock feathers
+
+        #Pressing enter, the page updates and "!: Buy Peacock Feathers is an item"
+
+        #Enter use peacock feathres to make a fly
+
+        #Page updates
+
+        #Visit the custom URL
+
+        #all works
+
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
