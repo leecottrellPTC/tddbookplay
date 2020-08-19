1aa00e2d5eaf1b063f463158351fc59ab73a7a42
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2020-08-19 14:15:26 -0400

    Redirect after post. All items in template - end 0f Ch5

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 
@@ -8,4 +9,13 @@ def home_page(request):
     #pass   #just passes the test
     #if request.method == 'POST':
     #    return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', { 'new_item_text': request.POST.get('item_text', '')})
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+    # return render(request, 'home.html', { 'new_item_text': item.text})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text']) #save to the database
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
